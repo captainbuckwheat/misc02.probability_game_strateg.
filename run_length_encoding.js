b89df804493c7bb5345b9adfc8985c6322063f0e
AK <captainbuckwheat@gmail.com>
2016-04-28 17:14:06 -0700

    Create run_length_encoding.js

---
+++ run_length_encoding.js
@@ -0,0 +1,8 @@
+/*
+If we start with any digit d from 0 to 9 then d will remain indefinitely as the last digit of the sequence. For d different from 1, the sequence starts as follows:
+
+d, 1d, 111d, 311d, 13211d, 111312211d, 31131122211d, …
+
+(wikipedia: https://en.wikipedia.org/wiki/Run-length_encoding)
+
+*/
